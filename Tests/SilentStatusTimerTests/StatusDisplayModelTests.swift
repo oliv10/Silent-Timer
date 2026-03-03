aed9eb7544ebcc8bd156a579e00c8c0760aa2570
Oliver Scotten <revi99@me.com>
2026-03-03 09:13:52 -0500

    feat: add hide-countdown setting with running-state icon

---
+++ Tests/SilentStatusTimerTests/StatusDisplayModelTests.swift
@@ -0,0 +1,48 @@
+import XCTest
+@testable import SilentStatusTimer
+
+final class StatusDisplayModelTests: XCTestCase {
+    func testIdleAndHideOffShowsIdleIconWithoutTitle() {
+        let model = StatusDisplayModel.make(
+            isRunning: false,
+            hideCountdownInMenuBar: false,
+            countdownLabel: nil
+        )
+
+        XCTAssertEqual(model.iconSymbolName, "timer")
+        XCTAssertEqual(model.menuBarTitle, "")
+    }
+
+    func testIdleAndHideOnShowsIdleIconWithoutTitle() {
+        let model = StatusDisplayModel.make(
+            isRunning: false,
+            hideCountdownInMenuBar: true,
+            countdownLabel: nil
+        )
+
+        XCTAssertEqual(model.iconSymbolName, "timer")
+        XCTAssertEqual(model.menuBarTitle, "")
+    }
+
+    func testRunningAndHideOffShowsRunningIconWithCountdownTitle() {
+        let model = StatusDisplayModel.make(
+            isRunning: true,
+            hideCountdownInMenuBar: false,
+            countdownLabel: "00:05:00"
+        )
+
+        XCTAssertEqual(model.iconSymbolName, "timer.circle.fill")
+        XCTAssertEqual(model.menuBarTitle, " 00:05:00")
+    }
+
+    func testRunningAndHideOnShowsRunningIconWithoutTitle() {
+        let model = StatusDisplayModel.make(
+            isRunning: true,
+            hideCountdownInMenuBar: true,
+            countdownLabel: "00:05:00"
+        )
+
+        XCTAssertEqual(model.iconSymbolName, "timer.circle.fill")
+        XCTAssertEqual(model.menuBarTitle, "")
+    }
+}
